f11feef9fb152bdcd45f8a12c8e461dd3f6e6e19
durken <git@durken.se>
2024-02-20 15:54:31 +0100

    add loading animation to 7seg

--- design/ip/7seg/seg_hw_ip.vhd
+++ design/ip/7seg/seg_hw_ip.vhd
@@ -36,22 +36,31 @@ end entity;
 
 architecture seg_hw_ip_rtl of seg_hw_ip is
 
-  -- internal constants --
-  constant addr_mask_c : std_ulogic_vector(31 downto 0) := std_ulogic_vector(to_unsigned(WB_ADDR_SIZE - 1, 32));
-  constant all_zero_c  : std_ulogic_vector(31 downto 0) := (others => '0');
-
-  -- address match --
-  signal access_req : std_ulogic;
-
-  -- dummy registers --
   type mm_reg_t is array (0 to (WB_ADDR_SIZE/4) - 1) of std_ulogic_vector(31 downto 0);
   type segled_t is array (0 to 5) of std_ulogic_vector(7 downto 0);
   type segval_t is array (0 to 5) of std_ulogic_vector(3 downto 0);
+
+  -- internal constants --
+  constant addr_mask_c    : std_ulogic_vector(31 downto 0) := std_ulogic_vector(to_unsigned(WB_ADDR_SIZE - 1, 32));
+  constant all_zero_c     : std_ulogic_vector(31 downto 0) := (others => '0');
+  constant loading_frames : segled_t                       := (
+  "11011111",
+  "11101111",
+  "11110111",
+  "11111011",
+  "11111101",
+  "11111110");
+
+  -- address match --
+  signal access_req : std_ulogic;
 
   signal segled_int   : segled_t;
   signal segval_int   : segval_t;
   signal signed_int   : std_ulogic;
   signal dec_int      : std_ulogic;
+  signal loading_int  : std_ulogic;
+  signal loading_cnt  : unsigned(21 downto 0);
+  signal loading_ind  : integer range 0 to 5;
   signal mm_reg       : mm_reg_t;
   signal mm_reg_neg   : std_ulogic_vector(31 downto 0);
   signal bcd_unsigned : std_ulogic_vector(23 downto 0);
@@ -74,9 +83,10 @@ architecture seg_hw_ip_rtl of seg_hw_ip is
 
 begin
 
-  mm_reg_neg <= std_ulogic_vector(-(signed(mm_reg(0))));
-  signed_int <= mm_reg(1)(0);
-  dec_int    <= mm_reg(1)(1);
+  mm_reg_neg  <= std_ulogic_vector( - (signed(mm_reg(0))));
+  signed_int  <= mm_reg(1)(0);
+  dec_int     <= mm_reg(1)(1);
+  loading_int <= mm_reg(1)(2);
 
   -- Sanity Checks --------------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
@@ -116,6 +126,24 @@ begin
     end if;
   end process rw_access;
 
+  -- counter for loading animation
+  process (wb_clk_i, wb_rstn_i)
+  begin
+    if wb_rstn_i = '0' then
+      loading_cnt <= (others => '0');
+      loading_ind <= 0;
+    elsif rising_edge(wb_clk_i) then
+      loading_cnt <= loading_cnt + 1;
+      if (loading_cnt = 0) then
+        if (loading_ind = 5) then
+          loading_ind <= 0;
+        else
+          loading_ind <= loading_ind + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+
   -- Split up data into seperate 7seg display values
   -- Output data
   process (wb_clk_i, wb_rstn_i)
@@ -128,13 +156,22 @@ begin
       segled4_o <= (others => '1');
       segled5_o <= (others => '1');
     elsif rising_edge(wb_clk_i) then
-      segled0_o <= segled_int(0); 
-      segled1_o <= segled_int(1); 
-      segled2_o <= segled_int(2); 
-      segled3_o <= segled_int(3); 
-      segled4_o <= segled_int(4); 
-      segled5_o <= segled_int(5); 
-      
+      if (loading_int = '1') then
+        segled0_o <= loading_frames(loading_ind);
+        segled1_o <= loading_frames(loading_ind);
+        segled2_o <= loading_frames(loading_ind);
+        segled3_o <= loading_frames(loading_ind);
+        segled4_o <= loading_frames(loading_ind);
+        segled5_o <= loading_frames(loading_ind);
+      else
+        segled0_o <= segled_int(0);
+        segled1_o <= segled_int(1);
+        segled2_o <= segled_int(2);
+        segled3_o <= segled_int(3);
+        segled4_o <= segled_int(4);
+        segled5_o <= segled_int(5);
+      end if;
+
       if (signed_int = '1' and mm_reg(0)(31) = '1') then
         segled5_o <= "10111111"; -- minus sign
         if (dec_int = '1') then
